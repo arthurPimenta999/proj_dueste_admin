e117ec5705b60d71ccff72e59cbe8fa64187150a
Arthur Pimenta <134031800+arthurPimenta999@users.noreply.github.com>
2025-03-05 22:09:52 -0300

    .

--- apis/firebaseConfig.tsx
+++ apis/firebaseConfig.tsx
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDy2KiQXzy0Ce5CuR83G_LE6UxJLYsWFiA",
-  authDomain: "projetodueste.firebaseapp.com",
-  databaseURL: "https://projetodueste-default-rtdb.firebaseio.com",
-  projectId: "projetodueste",
-  storageBucket: "projetodueste.appspot.com",
-  messagingSenderId: "989713015226",
-  appId: "1:989713015226:web:2185390e6f3eeb4ac93d83",
-  measurementId: "G-BH2VJ1FDZN",
+  apiKey: "local",
+  authDomain: "local",
+  databaseURL: "local",
+  projectId: "local",
+  storageBucket: "local",
+  messagingSenderId: "local",
+  appId: "local",
+  measurementId: "local"
 };
 
 export const app = initializeApp(firebaseConfig);
